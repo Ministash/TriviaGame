76d81d6987cca38aab1afbed7cfaedaf3b8c7aab
Bob Ross <ministash14@gmail.com>
2018-05-29 19:43:18 -0700

    Finishing bugs with first question area

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,5 @@
-var timer = 5;
+var timer = 10;
+var backgroundTimer = 38;
 var intervalId;
 var startRedo = $("#startButton")
 
@@ -16,9 +17,6 @@ $("#startButton").on("click", function(){
             
       );
 
-      if(timer === 61){
-        $("#upperText").remove();
-      }
     questionOnePage();
     timerStart();
 })
@@ -35,22 +33,87 @@ function decrease(){
     $("#timerDiv").html("Time Remaining: " + timer + " Seconds");
 
     if(timer === 0){
-        $("#timerDiv").html("You Lose!")
         clearInterval(intervalId);
-        $("#contentWrapper").html(startRedo)
-        $("#upperText").html("You lost! Want to play again?")
-        
-        gameStart();
-        timer = 61;
-
+        $("#questionsDiv").html("You waited too long! The correct answer is rabbies!")
+        $("#questionWrapper").html("")
+        // $("#contentWrapper").append(
+        //     $("<div/>")
+        //     .attr('id', 'wrongGif')
+        // )
+ 
+        //Need to tell it to go to the next page
     }
 }
 
 function questionOnePage(){
+
     $("#contentWrapper").append(
         $("<div/>")
           .attr('id', 'questionsDiv')
           .text("Which disease causes hallucinations and fear of water shortly before death?")
     );
+
+    $("#questionWrapper").append(
+        $("<div/>")
+        .attr('id', 'questionOne')
+        .attr('class', 'wrongAnswer')
+        .addClass('wrongAnswer x' )
+        .text("Lyme disease")
+    );
+    
+    $("#questionWrapper").append(
+        $("<div/>")
+        .attr('id', 'questionOne')
+        .attr('class', 'wrongAnswer')
+        .addClass('wrongAnswer x' )
+        .text("Malaria")
+
+    );
+
+    $("#questionWrapper").append(
+        $("<div/>")
+        .attr('id', 'questionOne')
+        .attr('class', 'correctAnswer')
+        .addClass('wrongAnswer x' )
+        .text("Rabbies")
+
+    );
+
+    $("#questionWrapper").append(
+        $("<div/>")
+        .attr('id', 'questionOne')
+        .addClass('correctAnswer x' )
+        .text("Ebola")
+
+    );
+    
+    $(".correctAnswer").on("click", function(){
+        $("#questionWrapper").html("")
+        $("#questionsDiv").html("That is the correct answer! Congratulations!")
+        $("#contentWrapper").append(
+            $("<div/>")
+            .attr('id', 'rightGif')
+        )
+
+                // if(timer > 5){
+            
+        // }
+    });
+    
+    $(".wrongAnswer").on("click", function(){
+        $("#questionWrapper").html("")
+        $("#questionsDiv").html("That is the wrong answer! The correct is rabbies!")
+        $("#contentWrapper").append(
+            $("<div/>")
+            .attr('id', 'wrongGif')
+        )
+
+
+
+        // if(timer > 5){
+            
+        // }
+    });
+
 
 }
