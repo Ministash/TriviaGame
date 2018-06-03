38a3cf389e46cca2c320fa4764f2bf6fa0bd8eb0
Bob Ross <ministash14@gmail.com>
2018-06-02 22:18:45 -0700

    Change the amount of time for each gif on splash screen

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -102,7 +102,7 @@ function buttons() {
 
         if (timer > -1) {
             clearInterval(intervalId);
-            backgroundTimer = 4;
+            backgroundTimer = 7;
             timer = 0;
             $("#timerDiv").html("Time Remaining: " + timer + " Seconds");
         };
@@ -124,7 +124,7 @@ function buttons() {
 
         if (timer > -1) {
             clearInterval(intervalId);
-            backgroundTimer = 6;
+            backgroundTimer = 7;
             timer = 0;
             $("#timerDiv").html("Time Remaining: " + timer + " Seconds");
         }
